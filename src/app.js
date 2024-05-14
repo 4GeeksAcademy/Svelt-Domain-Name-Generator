bd9e00fbcdfdf99c5f0b529bca8085cb862a39e1
chaolinespo <162933423+chaolinespo@users.noreply.github.com>
2024-05-14 19:16:26 +0000

    project completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+import "./index.html";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomainNames() {
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let ends = [".com", ".net"];
+  const domainNames = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+      for (const noun of nouns) {
+        for (const end of ends) {
+          const domainName = `${pronoun}${adjective}${noun}${end}`;
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+  return domainNames;
+}
+
+window.onload = () => {
+  const button = document.querySelector("#domainbutton");
+  const p = document.querySelector("#domain");
+  button.onclick = () => {
+    p.innerHTML = generateDomainNames().join(
+      '<marquee><i class="fa-solid fa-flag-checkered" style="color: #511F1F;"></i></marquee>'
+    );
+  };
 };
+
+
+
